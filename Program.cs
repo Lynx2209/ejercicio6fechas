1e0e774cb1a09edb616ac01d82741179373f4f79
Lynx2209 <csi23-jconazo@colegioaltair.net>
2024-02-07 09:26:50 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Headers;
+
+namespace ejercicio6fecha
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Que mes desea (en numero) : ");
+            int mes = int.Parse(Console.ReadLine());
+            Console.WriteLine("Cuantos dias tiene tu mes");
+            int diasQueTieneElMes = int.Parse(Console.ReadLine());
+            int anyo = 2024;
+            Console.WriteLine("Que dia desea (en ingles) : ");
+            string diaDeLaSemana = Console.ReadLine();
+
+            for (int i = 1; i <= diasQueTieneElMes; i++)
+            {
+                DateTime fecha = new DateTime(anyo, mes, i);
+                if (fecha.DayOfWeek.Equals(diaDeLaSemana))
+                {
+                    Console.WriteLine(fecha);
+                }
+            }
+
+        }
+    }
+}
